2e442b8614e738286a1957e130f50bc96afe033a
sourav <souravramesh39@gmail.com>
2024-05-06 10:32:29 +0530

    Fixed image loading issue

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,4 +1,7 @@
+import os
+
 from django.db import models
+from PIL import Image
 
 
 class Restaurants(models.Model):
@@ -6,10 +9,23 @@ class Restaurants(models.Model):
     delivery_time = models.IntegerField()
     cuisine_type = models.CharField(max_length=50)
     place = models.CharField(max_length=20)
-    image_url = models.URLField(blank=True)
+    image = models.ImageField(default="default.jpg", upload_to="restaurant_pics")
 
     def __str__(self):
         return self.name
+
+    # function to resize upload image if it exceeds 600px
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        # Check if the image field is not empty and the file exists
+        if self.image and hasattr(self.image, 'path') and os.path.exists(self.image.path):
+            img = Image.open(self.image.path)
+
+            if img.height > 600 or img.width > 600:
+                output_size = (600, 600)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
 
 
 class MenuItem(models.Model):
@@ -17,9 +33,20 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image_url = models.URLField(blank=True)
+    image = models.ImageField(default="default.jpg", upload_to="dishes_pics")
 
     def __str__(self):
         return self.name
 
+    # function to resize upload image if it exceeds 600px
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
+        # Check if the image field is not empty and the file exists
+        if self.image and hasattr(self.image, 'path') and os.path.exists(self.image.path):
+            img = Image.open(self.image.path)
+
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
