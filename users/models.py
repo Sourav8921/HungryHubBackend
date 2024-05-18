893423f2dbda10b63dab5fee6d65097a2dc331d4
sourav <souravramesh39@gmail.com>
2024-05-18 17:00:35 +0530

    created delivery address model

--- users/models.py
+++ users/models.py
@@ -8,3 +8,11 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class DeliveryAddress(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    address_line1 = models.CharField(max_length=200)
+    address_line2 = models.CharField(max_length=200, blank=True, null=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
