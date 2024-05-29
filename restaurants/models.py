27b1d72aaea81f7c8028bd8618321965422c157c
sourav <souravramesh39@gmail.com>
2024-05-29 22:41:40 +0530

    added indexes to most searched fields in models

--- restaurants/models.py
+++ restaurants/models.py
@@ -36,6 +36,9 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(default="default.jpg", upload_to="dishes_pics")
 
+    class Meta:
+        indexes = [models.Index(fields=["name"])]
+
     def __str__(self):
         return self.name
 
