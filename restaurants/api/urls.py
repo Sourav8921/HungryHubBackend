57e0a629e6dfed3a9cb975313bd8be8765ca2a61
sourav <souravramesh39@gmail.com>
2024-06-09 12:00:38 +0530

    Deleted order item and switched back to old setup

--- restaurants/api/urls.py
+++ restaurants/api/urls.py
@@ -9,6 +9,5 @@ router.register(r'orders', OrderViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('menu-items/', MenuItemsByRestaurantAPIView.as_view(), name='menu_items_by_restaurant'),
-    # path('orders/', CreateOrderView.as_view(), name='create-order'),
     path('search/', search_menu_items, name='search_menu_items'),
 ]
